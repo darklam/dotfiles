a46c0087c4ddc537e2fed879c626669d909214d2
John Lamis <lamisjohn@gmail.com>
2026-08-12 00:57:57 +0300

    Show hidden and gitignored files in neo-tree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -13,6 +13,10 @@ return {
       filesystem = {
         follow_current_file = { enabled = true },
         hijack_netrw_behavior = "open_default",
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
       },
     },
   },
